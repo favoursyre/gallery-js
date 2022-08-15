a72ecf8bb177524b16b0aa2426c709dfb48a0453
favoursyre <favourforchrist91@gmail.com>
2022-08-15 19:21:58 +0100

    commit

---
+++ readme.md
@@ -0,0 +1,37 @@
+# Gallery-JS
+
+![content](https://drive.google.com/uc?export=download&id=1-zgZ3NMTOju2RupcfUodLQLxTn8H0HjX)
+
+## Table of Contents
+
+1. [Overview](#overview)
+2. [Languages](#languages)
+3. [Technologies](#technologies)
+4. [Installations](#installations)
+5. [Run](#run)
+
+## Overview
+
+A simple script that displays picture in a sort of gallery form
+
+## Languages
+
+- HTML
+- CSS
+- Javascript
+
+## Technologies
+
+- Custom JS
+
+## Installations
+
+```bash
+git clone https://github.com/favoursyre/blurry-loader.git && cd blurry-loader
+```
+
+## Run
+
+```bash
+start ./index.html
+```
---
+++ script.js
@@ -0,0 +1,31 @@
+//I want to learn how to create a gallery
+
+// $(document).ready(function () {
+//     $(".carousel-item").each(function () {
+//         var itemTitle = $(".carousel")
+
+//         // Add a class on mouse enter in the grid-item
+//         $(this).on("mouseenter", function () {
+//             if ($(this).data("title")) {
+//                 itemTitle.html("<span>" + $(this).data("title") + "</span>");
+//                 itemTitle.addClass("visible");
+//                 console.log("Mouse is in")
+//             }
+  
+//         //  Move title on mouse move
+//         $(document).on("mousemove", function (e) {
+//           itemTitle.css({
+//             left: e.clientX - 10,
+//             top: e.clientY + 25,
+            
+//           });
+//           //console.log("mouse moving")
+//         });
+//       });
+  
+//       // Remove Class on mouse leave from grid-item
+//       $(this).on("mouseleave", function () {
+//         itemTitle.removeClass("visible");
+//       });
+//     });
+  });
